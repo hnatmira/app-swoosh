be5f443732c3be15bb6636c8516bd166c31645e0
Miroslav Hnát <miroslav.hnat@adastragrp.com>
2019-03-09 15:18:08 +0100

    Add logic on button for select logic

---
+++ app-swoosh/app-swoosh/Model /Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Miroslav Hnát on 09/03/2019.
+//  Copyright © 2019 Miroslav Hnát. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
