80edb34190b46077485e1484dac3957d0eebcc6f
Miroslav Hnát <miroslav.hnat@adastragrp.com>
2019-05-06 08:12:47 +0800

    Add things from 7 lesson

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -19,6 +19,7 @@ class LeagueVC: UIViewController {
         super.viewDidLoad()
         
         player = Player()
+        print("1", player.desiredLeague)
     }
     
     
@@ -46,14 +47,10 @@ class LeagueVC: UIViewController {
     }
     
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
     
 }
